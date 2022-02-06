58264db9661ea51825e6cdfbf97a66b8f0ebc862
Chamsol Yoon <dantert@gmail.com>
2022-02-06 16:09:11 +0900

    Initial commit

---
+++ src/test/java/io/hkarling/hello/SpringFrameworkIntroductionApplicationTests.java
@@ -0,0 +1,13 @@
+package io.hkarling.hello;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringFrameworkIntroductionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
