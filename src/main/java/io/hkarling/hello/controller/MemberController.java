e733555ddd79a04940f095ad18773199c73629ae
Chamsol Yoon <dantert@gmail.com>
2022-02-08 00:06:11 +0900

    회원 관리 예제 - 웹 MVC 개발

--- src/main/java/io/hkarling/hello/controller/MemberController.java
+++ src/main/java/io/hkarling/hello/controller/MemberController.java
@@ -1,8 +1,13 @@
 package io.hkarling.hello.controller;
 
+import io.hkarling.hello.domain.Member;
 import io.hkarling.hello.service.MemberService;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class MemberController {
@@ -13,5 +18,30 @@ public class MemberController {
     @Autowired // 스프링이 연관된 객체를 스프링 컨테이너에서 찾아서 넣어준다. 이렇게 객체 의존관계를 외부에서 넣어주는 것을 DI, 의존성 주입이라 한다.
     public MemberController(MemberService memberService) { // MemberService is not registered as Spring Bean -> @Service 추가
         this.memberService = memberService;
+    }
+
+
+    @GetMapping("/members/new")
+    public String createForm() {
+        return "members/createMemberForm";
+    }
+
+    @PostMapping("/members/new")
+    public String create(MemberForm form) {
+        Member member = new Member();
+        member.setName(form.getName());
+
+        memberService.join(member);
+
+        return "redirect:/";
+    }
+
+    @GetMapping("/members")
+    public String list(Model model) {
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members", members);
+
+        return "members/memberList";
+
     }
 }
